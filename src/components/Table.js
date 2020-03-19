2c806b3beffd503e150ab924650eeab8ff03ef5a
Arjun Chauhan <arjun99.chauhan@wesence.com>
2020-03-19 14:52:01 +0530

    initial commit

---
+++ src/components/Table.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Table = props => {
+  const { data, loading } = props;
+  return (
+    <table>
+      <thead>LOL</thead>
+      <tbody>NOOOOO</tbody>
+    </table>
+  );
+};
+
+export default Table;
